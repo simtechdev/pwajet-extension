dfe7452a7da65d5974fdff1a5d16d1bda7a0aa35
Mikhail Shemenev <mshemenev@simtechdev.com>
2020-08-17 17:27:40 +0400

    remove debug information;

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,8 @@ const productCode = (props: any) => (
     <ProductCode {...props} />
   </React.Suspense>
 )
-console.log('connected asd dd dd')
+
 const init = () => {
-  console.log('init called')
   pwajet.core.renderSubscriber.on('render-element.product/grid-item/ProductGridItem', event => {
     event.appendTo('div.b-product-grid-item__body', productCode)
   })
